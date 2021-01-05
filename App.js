730791882b4720dcc794532a7dd72d775d1b77f5
Pavel Kocman <kocmanpavel@gmail.com>
2021-01-05 19:06:06 +0100

    Add random background color, Button, jokes fetching

--- App.js
+++ App.js
@@ -1,21 +1,84 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useCallback, useEffect, useState } from "react";
+
+import { Button } from "./src/components/Button";
+import { StatusBar } from "expo-status-bar";
+import styled from "styled-components/native";
 
 export default function App() {
+  const [backgroundColor, setBackgroundColor] = useState("#000000");
+  const [quote, setQuote] = useState();
+  const [isLoadingQuote, setIsLoadingQuote] = useState("false");
+
+  const generateColor = () => {
+    const red = Math.floor(Math.random() * 256);
+    const green = Math.floor(Math.random() * 256);
+    const blue = Math.floor(Math.random() * 256);
+    return `rgb(${red},${green},${blue})`;
+  };
+  const handleFetchQuote = useCallback(() => {
+    setIsLoadingQuote(true);
+    fetch("https://icanhazdadjoke.com/", {
+      headers: {
+        Accept: "application/json",
+      },
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        setQuote(data.joke);
+        setBackgroundColor(generateColor());
+        setIsLoadingQuote(false);
+      });
+  }, []);
+
+  useEffect(() => {
+    handleFetchQuote();
+    return () => {};
+  }, [handleFetchQuote]);
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <Root {...{ backgroundColor }}>
+      {!quote ? (
+        <QuoteWrapper>
+          <Quote>Loading...</Quote>
+        </QuoteWrapper>
+      ) : (
+        <>
+          <QuoteWrapper>
+            <Quote>{quote}</Quote>
+          </QuoteWrapper>
+          <ButtonWrapper>
+            <Button
+              text={isLoadingQuote ? "Loading..." : " Next Quote"}
+              onPress={handleFetchQuote}
+            />
+          </ButtonWrapper>
+          <StatusBar style='auto' />
+        </>
+      )}
+    </Root>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+const Root = styled.View`
+  flex: 1;
+  background-color: ${({ backgroundColor }) => backgroundColor};
+  padding: 50px 20px;
+`;
+
+const QuoteWrapper = styled.View`
+  align-items: center;
+  flex: 1;
+  justify-content: center;
+  border-radius: 30px;
+`;
+
+const ButtonWrapper = styled.View``;
+
+const Quote = styled.Text`
+  color: #fff;
+  font-weight: 600;
+  font-size: 20px;
+  background-color: #000;
+  padding: 10px;
+  border-radius: 30px;
+`;
---
+++ src/components/Button.js
@@ -0,0 +1,27 @@
+import React from "react";
+import styled from "styled-components/native";
+
+export const Button = ({
+  text,
+  textColor = "#fff",
+  backgroundColor = "#000",
+  onPress,
+}) => (
+  <Root {...{ backgroundColor, onPress }}>
+    <Text {...{ textColor }}>{text}</Text>
+  </Root>
+);
+
+const Root = styled.TouchableOpacity`
+  background: ${({ backgroundColor }) => backgroundColor};
+  padding: 15px;
+  border-radius: 10px;
+  align-items: center;
+  border: 1px solid #fff;
+`;
+
+const Text = styled.Text`
+  color: ${({ textColor }) => textColor};
+  font-weight: 600;
+  letter-spacing: 2px;
+`;
